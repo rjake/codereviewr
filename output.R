f1a862b970178799677c52d418f5f031c83b426a
rjake <rjake@sas.upenn.edu>
2018-06-02 17:10:28 -0400

    create parse_it function, first pass

--- output.R
+++ output.R
@@ -2,5 +2,4 @@ library(tidyverse)
 model_name <- "a4"
 a <- mpg %>% mutate(ideal = model_name) %>% filter(model == ideal) %>% group_by(manufacturer) %>% count(cyl) %>% ungroup()
 fn <- function(x) { if(1 == 1){ 1}  else { 2} } 
-p <- paste0("here is one string of text", fn, "another string of text")
 x <- list(c(2, 3, 5), c("aa", "bb", "cc", "dd", "ee"), c(TRUE, FALSE, TRUE, FALSE, FALSE), 3) 
