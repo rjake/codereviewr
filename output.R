14ab22d83691970cbfacb76486097b045eeda86d
rjake <rjake@sas.upenn.edu>
2018-06-03 12:00:36 -0400

    update test doc

--- output.R
+++ output.R
@@ -1,5 +1,8 @@
 library(tidyverse)
-model_name <- "a4"
-a <- mpg %>% mutate(ideal = model_name) %>% filter(model == ideal) %>% group_by(manufacturer) %>% count(cyl) %>% ungroup()
-fn <- function(x) { if(1 == 1){ 1}  else { 2} } 
-x <- list(c(2, 3, 5), c("aa", "bb", "cc", "dd", "ee"), c(TRUE, FALSE, TRUE, FALSE, FALSE), 3) 
+model_name <- "pickup"
+mpg <- mpg
+models <- mpg %>% mutate(model_new = ifelse(str_detect(model, model_name), model_name, str_extract(model, "\\w+"))) %>% filter(year == 1999) %>% group_by(model_new) %>% count(sort = T) %>% ungroup()
+over_5 <- function(x) { x > 5} 
+models$over_5 <- over_5(models$n)
+edit_models <- models %>% mutate(over_10 = over_5(n/2))
+make_list <- list(edit_models$model_new, c(1999, 2000), TRUE) 
